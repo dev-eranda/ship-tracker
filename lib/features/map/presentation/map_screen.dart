c1bf0d1870e21bf2ad2da94d4f4039a430dc4da8
dev-eranda <emsamarasinghe@gmail.com>
2026-09-10 16:15:31 +0530

    Reduce maximum zoom level on map for better usability and update splash screen configuration comments

--- lib/features/map/presentation/map_screen.dart
+++ lib/features/map/presentation/map_screen.dart
@@ -35,7 +35,7 @@ class _MapScreenState extends ConsumerState<MapScreen> {
               initialCenter: _initialCenter,
               initialZoom: 11,
               minZoom: 3,
-              maxZoom: 18,
+              maxZoom: 16,
               onTap: (_, __) {
                 ref.watch(selectedVesselProvider.notifier).state = null;
               },
